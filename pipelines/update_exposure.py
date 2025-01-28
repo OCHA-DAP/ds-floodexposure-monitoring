f75a0793b3eb4af0a533357ac93ee2b27f3f3ff9
hannahker <hannah.ker96@gmail.com>
2025-01-28 11:42:22 -0500

    remove code for creating rasterstats parquet files

--- pipelines/update_exposure.py
+++ pipelines/update_exposure.py
@@ -1,11 +1,7 @@
 from src.constants import ISO3S
-from src.datasources.floodscan import (
-    calculate_recent_flood_exposure_rasters,
-    calculate_recent_flood_exposure_rasterstats,
-)
+from src.datasources.floodscan import calculate_recent_flood_exposure_rasters
 
 if __name__ == "__main__":
     for iso3 in ISO3S:
         print(f"Processing {iso3}")
         calculate_recent_flood_exposure_rasters(iso3)
-        calculate_recent_flood_exposure_rasterstats(iso3)
--- src/datasources/floodscan.py
+++ src/datasources/floodscan.py
@@ -3,11 +3,10 @@ from datetime import datetime
 from pathlib import Path
 from typing import Literal
 
-import pandas as pd
 import xarray as xr
 from tqdm.auto import tqdm
 
-from src.datasources import codab, worldpop
+from src.datasources import worldpop
 from src.utils import blob
 
 DATA_DIR = Path(os.getenv("AA_DATA_DIR_NEW", ""))
@@ -119,138 +118,6 @@ def calculate_recent_flood_exposure_rasters(
         if verbose:
             print(f"uploading {blob_name}")
         blob.upload_cog_to_blob(blob_name, exposure.sel(date=date))
-
-
-def calculate_recent_flood_exposure_rasterstats(
-    iso3: str, clobber: bool = False, verbose: bool = False
-):
-    """
-    Calculate recent (current year onwards) flood exposure sums for a
-    given country. Only calculates for admin level 2.
-
-    Parameters
-    ----------
-    iso3: str
-        ISO3 code of the country
-    clobber: bool
-        Whether to overwrite existing data
-    verbose: bool
-        Whether to print progress of specific dates
-
-    Returns
-    -------
-
-    """
-    adm = codab.load_codab_from_blob(iso3, admin_level=2)
-    # check for existing exposure rasters
-    recent_exposure_rasters = [
-        x
-        for x in blob.list_container_blobs(
-            name_starts_with=f"{blob.PROJECT_PREFIX}/processed/"
-            f"flood_exposure/{iso3}/"
-        )
-        if x.endswith(".tif")
-    ]
-    # load existing exposure tabular data
-    blob_name = get_blob_name(iso3, "exposure_tabular")
-    try:
-        df_exp_adm_existing = blob.load_parquet_from_blob(blob_name)
-    except Exception as e:
-        print(e)
-        df_exp_adm_existing = pd.DataFrame(columns=["date"])
-
-    existing_dates = df_exp_adm_existing["date"].unique()
-    # filter to only unprocessed exposure rasters
-    unprocessed_exposure_rasters = [
-        x
-        for x in recent_exposure_rasters
-        if datetime.strptime(x.split("/")[-1][13:23], "%Y-%m-%d")
-        not in existing_dates
-        or clobber
-    ]
-    if clobber:
-        df_empty = pd.DataFrame(columns=["date"])
-        blob_name = get_blob_name(iso3, "exposure_tabular")
-        blob.upload_parquet_to_blob(blob_name, df_empty, index=False)
-
-    # break list of exposure rasters into chunks, to avoid memory issues
-    # chunk length is arbitrary, but seems to work fine
-    chunk_len = 100
-    exposure_raster_chunks = [
-        unprocessed_exposure_rasters[x : x + chunk_len]
-        for x in range(0, len(unprocessed_exposure_rasters), chunk_len)
-    ]
-
-    # iterate over chunks
-    for exposure_raster_chunk in tqdm(exposure_raster_chunks):
-        blob_name = get_blob_name(iso3, "exposure_tabular")
-        try:
-            df_exp_adm_existing = blob.load_parquet_from_blob(blob_name)
-        except Exception as e:
-            print(e)
-            df_exp_adm_existing = pd.DataFrame(columns=["date"])
-
-        existing_dates = df_exp_adm_existing["date"].unique()
-        if verbose:
-            print(existing_dates)
-
-        # stack up exposure rasters in chunk
-        das = []
-        for blob_name in tqdm(exposure_raster_chunk):
-            date_in = datetime.strptime(
-                blob_name.split("/")[-1][13:23], "%Y-%m-%d"
-            )
-            if date_in in existing_dates and not clobber:
-                if verbose:
-                    print(f"already processed for {date_in}, skipping")
-                continue
-            try:
-                da_in = blob.open_blob_cog(blob_name)
-                da_in["date"] = date_in
-                da_in = da_in.persist()
-                das.append(da_in)
-            except Exception as e:
-                print(e)
-                print(f"couldn't open {blob_name}")
-
-        if len(das) == 0:
-            print("all complete for chunk")
-            continue
-        ds_exp_recent = xr.concat(das, dim="date").squeeze(
-            dim="band", drop=True
-        )
-        if verbose:
-            print(ds_exp_recent)
-
-        # iterate over admin level 2 regions and calculate exposure sums
-        dfs = []
-        for pcode, row in tqdm(
-            adm.set_index("ADM2_PCODE").iterrows(), total=len(adm)
-        ):
-            da_clip = ds_exp_recent.rio.clip([row.geometry])
-            dff = (
-                da_clip.sum(dim=["x", "y"])
-                .to_dataframe(name="total_exposed")["total_exposed"]
-                .astype(int)
-                .reset_index()
-            )
-            dff["ADM2_PCODE"] = pcode
-            dfs.append(dff)
-
-        df_exp_adm_new = pd.concat(dfs, ignore_index=True)
-        if verbose:
-            print(df_exp_adm_new)
-
-        df_exp_adm_combined = pd.concat(
-            [df_exp_adm_existing, df_exp_adm_new], ignore_index=True
-        )
-        if verbose:
-            print(df_exp_adm_combined)
-
-        blob_name = get_blob_name(iso3, "exposure_tabular")
-        blob.upload_parquet_to_blob(
-            blob_name, df_exp_adm_combined, index=False
-        )
 
 
 def open_historical_floodscan():
